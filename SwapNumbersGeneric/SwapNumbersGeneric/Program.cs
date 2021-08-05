367d557dc21e0199015b118bd6182963b6b3f6ab
smomin156 <63006524+smomin156@users.noreply.github.com>
2021-08-05 18:43:17 +0530

    Add files via upload

---
+++ SwapNumbersGeneric/SwapNumbersGeneric/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwapNumbersGeneric
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SwapNumbers<int> objInt = new SwapNumbers<int>();
+            Console.WriteLine("Enter a number 1");
+            objInt.Var1= Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter a number 2");
+            objInt.Var2= Convert.ToInt32(Console.ReadLine());
+
+            objInt.Swap();
+            Console.WriteLine("num1= "+objInt.Var1);
+            Console.WriteLine("num2= "+objInt.Var2);
+            Console.WriteLine("-----string--");
+            SwapNumbers<string> objString = new SwapNumbers<string>();
+            Console.WriteLine("Enter a string 1");
+            objString.Var1 = Console.ReadLine();
+            Console.WriteLine("Enter a string 2");
+            objString.Var2 = Console.ReadLine();
+
+            objString.Swap();
+            Console.WriteLine("string1= " + objString.Var1);
+            Console.WriteLine("string2= " + objString.Var2);
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SwapNumbersGeneric/SwapNumbersGeneric/SwapNumbers.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwapNumbersGeneric
+{
+    class SwapNumbers<Tnum>
+    {
+
+        private Tnum var1;
+        private Tnum var2;
+
+        public Tnum Var1
+        {
+            get { return var1; }
+            set { var1 = value; }
+        }
+        public Tnum Var2
+        {
+            get { return var2; }
+            set { var2 = value; }
+        }
+        public void Swap()
+        {
+            Tnum temp = Var1;
+            Var1 = Var2;
+            Var2 = temp;
+        }
+
+        
+    }
+}
